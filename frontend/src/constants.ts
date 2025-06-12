3eb616e5c5843c155dd207275ffcc662339ee17e
Iamorphouz <zishuz44@gmail.com>
2025-06-12 13:51:01 +0530

    add src files

---
+++ frontend/src/constants.ts
@@ -0,0 +1,19 @@
+export const SWATCHES = [
+  "#ffffff", // white
+  "#000000", // black
+  "#ee3333", // red
+
+  "#e64980", // pink
+
+  "#be4bdb", // purple
+
+  "#228be6", // blue
+
+  "#3333ee", // dark blue
+
+  "#40c057", // green
+
+  "#fab005", // yellow
+
+  "#fd7e14", // orange
+]
